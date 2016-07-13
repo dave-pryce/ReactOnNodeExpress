7a3c988ed72f7f7c508dcc280c4a1508850e4405
David Pryce <dpryce@seek.com.au>
2016-07-14 08:13:20 +1000

    move html into components.js

--- public/components.js
+++ public/components.js
@@ -2,6 +2,17 @@ class CitiesBox extends React.Component {
   render() {
     return (<div className="container-fluid well">
             <h1 className="text-center">Express Cities App with a React Front End</h1>
+
+            <div class='container-fluid well'>
+            <h2>Add new City</h2>
+            <form>
+              <input name="name" placeholder="City Name" class="form-control"></input>
+              <input name="description" placeholder="City Description" class="form-control"></input>
+              <input type ="Submit" class='btn btn-default'></input>
+            </form>
+            </div>
+
+
             </div>);
   }
 }
