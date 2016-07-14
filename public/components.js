42dbc061b20547dd85a9531a8443cb199c922765
David Pryce <dpryce@seek.com.au>
2016-07-15 08:39:40 +1000

    using props.

--- public/components.js
+++ public/components.js
@@ -2,21 +2,35 @@ class CitiesBox extends React.Component {
   render() {
     return (<div>
                 <div className="container-fluid well">
-                <h1 className="text-center">Express Cities App with a React Front End</h1>
+                <h1 className="text-center">Cities App</h1>
+                <p className="text-center">React front-end / Express Node Back-end</p>
                 </div>
 
-                <Cities/>
+                <CitiesList data={this.props.data}/>
                 <CitiesForm/>
             </div>);
   }
 }
 
 
-class Cities extends React.Component {
+class CitiesList extends React.Component {
   render() {
-    return (<div className="container-fluid well">
-          <h3><ul>XXX</ul></h3>
+    return (
+          <div className="container-fluid well">
+          <h3>Cities List</h3>
+          <ul>
+          <City name="Melbourne" description="Hipsters"/>
+          </ul>
           </div>);
+  }
+}
+
+
+class City extends React.Component {
+  render() {
+    return (<div>
+      <p>{this.props.name} {this.props.description}</p>
+    </div>);
   }
 }
 
@@ -24,7 +38,7 @@ class Cities extends React.Component {
 class CitiesForm extends React.Component {
   render() {
   return  (<div className='container-fluid well'>
-    <h2>Add new City</h2>
+    <h3>Add new City</h3>
     <form>
       <input name="name" placeholder="City Name" className="form-control"></input>
       <input name="description" placeholder="City Description" className="form-control"></input>
@@ -34,6 +48,13 @@ class CitiesForm extends React.Component {
   }
 }
 
+
+//var data = [
+//  {id: 1, name: "Melbourne", description: "Hipsters"},
+//  {id: 2, name: 'Sydney', description:  'Surfers'},
+//  {id: 3, name: 'Brisbane', description: 'Vegas'},
+//  {id: 4, name: 'Adelaide', description: 'Churches'
+//];
 
 
 let output = document.getElementById("cities-box");
