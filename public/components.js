e7a649381d13636d5eeb0df84b38ef489ed309cd
David Pryce <dpryce@seek.com.au>
2016-07-15 07:42:16 +1000

    split up html into 3 react components.

--- public/components.js
+++ public/components.js
@@ -1,21 +1,40 @@
 class CitiesBox extends React.Component {
   render() {
-    return (<div className="container-fluid well">
-            <h1 className="text-center">Express Cities App with a React Front End</h1>
+    return (<div>
+                <div className="container-fluid well">
+                <h1 className="text-center">Express Cities App with a React Front End</h1>
+                </div>
 
-            <div class='container-fluid well'>
-            <h2>Add new City</h2>
-            <form>
-              <input name="name" placeholder="City Name" class="form-control"></input>
-              <input name="description" placeholder="City Description" class="form-control"></input>
-              <input type ="Submit" class='btn btn-default'></input>
-            </form>
-            </div>
-
-
+                <Cities/>
+                <CitiesForm/>
             </div>);
   }
 }
+
+
+class Cities extends React.Component {
+  render() {
+    return (<div className="container-fluid well">
+          <h3><ul>XXX</ul></h3>
+          </div>);
+  }
+}
+
+
+class CitiesForm extends React.Component {
+  render() {
+  return  (<div className='container-fluid well'>
+    <h2>Add new City</h2>
+    <form>
+      <input name="name" placeholder="City Name" className="form-control"></input>
+      <input name="description" placeholder="City Description" className="form-control"></input>
+      <input type ="Submit" className='btn btn-default'></input>
+    </form>
+    </div>);
+  }
+}
+
+
 
 let output = document.getElementById("cities-box");
 ReactDOM.render(<CitiesBox/>, output);
